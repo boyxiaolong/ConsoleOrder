1412780e7ff337253c84afbedeb21bcd21a9d595
boyxiaolong <boyhailong@gmai.com>
2016-05-04 17:03:40 +0800

    add

--- ConsoleOrder/ViewController.swift
+++ ConsoleOrder/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("begin to load")
         var zones = AllZones()
         zones.loadFile("/waimai_chaoren_json_data.txt")
     }
